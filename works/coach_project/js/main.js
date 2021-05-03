88b9f52c3c5f07675b7b1cc41ea1c64dc5abdcc0
Oleksandr Serhiienko <38429862+seralex94@users.noreply.github.com>
2021-05-03 13:49:36 +0300

    Add files via upload

--- works/coach_project/js/main.js
+++ works/coach_project/js/main.js
@@ -17,12 +17,35 @@ $(document).ready(function() {
   });
 });
 
-$(".js-open-requst_popup, .js-open-buy_popup").click(function() {
-  $(".popup").fadeIn(300);
+$(".request-open-popup").click(function() {
+  $(".request-popup").fadeIn(300);
+  $('body').css('overflow-y', 'hidden');
+});
+
+$(".buy-open-popup").click(function() {
+  $(".buy-popup").fadeIn(300);
+  $('body').css('overflow-y', 'hidden');
+});
+
+
+$(".js-close-popup").click(function() {
+  $(".request-popup, .buy-popup").fadeOut(0);
+  $('body').css('overflow-y', 'visible');
+});
+
+$(".js-scroll-to-top").click(function() {
+  $("html, body").animate({ scrollTop: 0}, "medium");
+  return false;
+});
+
+/* $(".js-open-request_popup").click(function() {
+  $(".request_popup").fadeIn(300);
   $('body').css('overflow-y', 'hidden');
 });
 
 $(".js-close-popup").click(function() {
-  $(".popup").fadeOut(0);
+  $(".request_popup").fadeOut(0);
   $('body').css('overflow-y', 'visible');
-});
+}); */
+
+
