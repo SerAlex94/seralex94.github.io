de800095c3d61c41bb1a28a2f69a6ff44571fa3a
Oleksandr Serhiienko <38429862+seralex94@users.noreply.github.com>
2021-05-03 14:17:30 +0300

    Add files via upload

--- works/coach_project/js/main.js
+++ works/coach_project/js/main.js
@@ -8,35 +8,50 @@ $(document).ready(function(){
       }
     ]
   });
-});
-
-$(document).ready(function() {
+  
   $('.navi__btn').click(function(){
-      $(this).toggleClass('active');
-      $('.navi').slideToggle(400);
+    $(this).toggleClass('active');
+    $('.navi').slideToggle(400);
+  });
+
+  $(".request-open-popup").click(function() {
+    $(".request-popup").fadeIn(300);
+    $('body').css('overflow-y', 'hidden');
+  });
+
+  $(".buy-open-popup").click(function() {
+    $(".buy-popup").fadeIn(300);
+    $('body').css('overflow-y', 'hidden');
+  });
+
+  $(".js-close-popup").click(function() {
+    $(".request-popup, .buy-popup").fadeOut(0);
+    $('body').css('overflow-y', 'visible');
+  });
+
+  $(".js-scroll-to-top").click(function() {
+    $("html, body").animate({ scrollTop: 0}, "medium");
+    return false;
+  });
+
+  //E-mail Ajax Send
+  $("popup__form").submit(function() { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "./mail.php", //Change
+      data: th.serialize()
+    }).done(function() {
+      alert("Thank you!");
+      setTimeout(function() {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
   });
 });
 
-$(".request-open-popup").click(function() {
-  $(".request-popup").fadeIn(300);
-  $('body').css('overflow-y', 'hidden');
-});
-
-$(".buy-open-popup").click(function() {
-  $(".buy-popup").fadeIn(300);
-  $('body').css('overflow-y', 'hidden');
-});
-
-
-$(".js-close-popup").click(function() {
-  $(".request-popup, .buy-popup").fadeOut(0);
-  $('body').css('overflow-y', 'visible');
-});
-
-$(".js-scroll-to-top").click(function() {
-  $("html, body").animate({ scrollTop: 0}, "medium");
-  return false;
-});
 
 /* $(".js-open-request_popup").click(function() {
   $(".request_popup").fadeIn(300);
